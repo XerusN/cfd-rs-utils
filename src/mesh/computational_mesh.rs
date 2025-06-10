bfae326085a0329ca236b277ee48d0872bf330fc
XerusN <134720540+XerusN@users.noreply.github.com>
2025-06-10 20:40:19 +0200

    fix normals in wrong direction

--- src/mesh/computational_mesh.rs
+++ src/mesh/computational_mesh.rs
@@ -39,7 +39,7 @@ impl Face {
         vertices_glob: &[Point2<f64>],
     ) -> Self {
         let area = line_length(&[vertices_glob[vertices[0].0], vertices_glob[vertices[1].0]]);
-        let normal = -line_normal(&[vertices_glob[vertices[0].0], vertices_glob[vertices[1].0]]);
+        let normal = line_normal(&[vertices_glob[vertices[0].0], vertices_glob[vertices[1].0]]);
 
         Face {
             vertices,
